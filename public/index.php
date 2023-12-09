9b4d89d5264d068954b8d96759c923538a96d8a9
anamachadocarolina <acarolina066@gmail.com>
2023-12-09 09:52:23 -0300

    Cria htaccess para url amigável

--- public/index.php
+++ public/index.php
@@ -3,8 +3,10 @@
 require_once('../app/application.php');
 use Controllers\Home;
 
-$controller = new Home();
-$controller->index();
+// $controller = new Home();
+// $controller->index();
+
+//echo $_GET['url']; testa se o arquivo .htaccess está funcionando
 /*use Core\View;
 
 $tela = new View('produtos.produtos','main');  '../app/Views/produtos.php'    Para carregar outras paginas, basta troca o nome main pelo nome da pagina desejada 
