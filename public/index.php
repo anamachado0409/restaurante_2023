c4d34f8183553af1596e70823afe6e93749e9252
anamachadocarolina <acarolina066@gmail.com>
2023-12-09 23:20:11 -0300

    Cria estrutura inicial do Router

--- public/index.php
+++ public/index.php
@@ -2,12 +2,32 @@
 
 require_once('../app/application.php');
 use Controllers\Home;
+use Core\Router;
+
 
 // $controller = new Home();
 // $controller->index();
 
-//echo $_GET['url']; testa se o arquivo .htaccess está funcionando
-/*use Core\View;
+$url = '/';
+if(isset($_GET['url'])){
+    $url = $_GET['url'];
+}
 
+$router = Router::getRouterByUrl($url);
+
+if($router){
+    $controller = $router->getController();
+    $action = $router->getAction();
+    $controller = new $controller();
+    $controller->$action();
+} else {
+    die('page 404');
+}
+
+
+//echo $_GET['url']; testa se o arquivo .htaccess está funcionando
+
+
+/*use Core\View;
 $tela = new View('produtos.produtos','main');  '../app/Views/produtos.php'    Para carregar outras paginas, basta troca o nome main pelo nome da pagina desejada 
 $tela->show();*/
