81ee7ce66634b056b5dd952ef8271e2b4d81c3e8
anamachadocarolina <acarolina066@gmail.com>
2023-12-20 14:26:34 -0300

    Cria código do arquivo connection e testa erro de conexão

--- app/Configs/constantes.php
+++ app/Configs/constantes.php
@@ -2,6 +2,8 @@
 
 defined ('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(__DIR__ . '/../..'));
 
+defined ('APPLICATION_ENV') || define('APPLICATION_ENV', 'development'); //testing|development|production
+
 defined ('APPLICATION_URL') || define('APPLICATION_URL', 'http://restaurante.localhost');
 
 defined ('ADMIN_LTE') || define('ADMIN_LTE', APPLICATION_URL.'/adminlte');
@@ -17,3 +19,4 @@ defined ('TEMPLATE_DEFAULT') || define('TEMPLATE_DEFAULT', 'main');
 defined ('COMPOSER_PATH') || define('COMPOSER_PATH', APPLICATION_PATH . '/vendor');
 
 defined ('CONFIGS_PATH') || define('CONFIGS_PATH', APP_PATH . "/Configs");
+
--- public/teste.php
+++ public/teste.php
@@ -1,3 +1,10 @@
 <?php
 
 require_once('../app/application.php');
+
+use Core\Connection;
+
+$conn = Connection::getInstance();
+$stm = $conn->prepare('SHOW TABLES');
+$stm->execute();
+pre($stm->fetchall());
