97cf0922d443f57802e78a50e66dadfdd286b250
anamachadocarolina <acarolina066@gmail.com>
2023-12-26 20:06:20 -0300

    Refina model

--- public/teste.php
+++ public/teste.php
@@ -5,5 +5,15 @@ use Models\Produto;
 
 require_once('../app/application.php');
 
-$config = new Config(2);
-echo $config->name . " = " . $config->value;
+$model = new Produto(1);
+//$model->save(['name'=>'Root_User', 'value'=>'Joaquim']);
+$model->delete();
+
+all($model);
+
+function all($model){
+    $configs = $model->all();
+    array_walk($configs, function($config){
+    echo $config->id . " | " . $config->nome . " | " . $config->valor_un . "<hr>"; 
+    });
+}
