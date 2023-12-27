701688af7c65adbbfba1d5dc01f849c5294ca517
anamachadocarolina <acarolina066@gmail.com>
2023-12-27 17:15:23 -0300

    Separa aplicação do framework

--- public/teste.php
+++ public/teste.php
@@ -1,23 +1,9 @@
 <?php
 
-use Models\Config;
-use Models\Produto;
 
 require_once('../app/application.php');
 
-$model = new Produto(2);
-// $model->valor_un = 10;
-//$model->save();
-//$model->save(['name'=>'Root_User', 'value'=>'Joaquim']);
-//$model->delete();
+use Models\Config;
 
 
-pre($model->getData());
-// all($model);
-
-function all($model){
-    $configs = $model->all();
-    array_walk($configs, function($config){
-    echo $config->id . " | " . $config->nome . " | " . $config->valor_un . "<hr>"; 
-    });
-}
+pre(APPLICATION_NAME);
